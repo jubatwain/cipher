8c84bbac9a61c99ccc54313d3bc48188c135e352
jubatwain <102503462+jubatwain@users.noreply.github.com>
2023-01-11 10:17:22 +0300

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,7 @@
+text = input("Give me text: ") #normal text
+key = int(input("Key: ")) 
+output = ""
+for char in text: #for each character in the text entered,
+    #we're gonna add it to the output string
+    output += chr(ord(char) ^ key) #ord finds the scode. chr turns scode back to character
+print(output)
